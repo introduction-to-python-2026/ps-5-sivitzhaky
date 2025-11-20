5dd35e4e4bc7195855c76e0494cf030eac46f338
sivitzhaky <itzhaky.sivan@gmail.com>
2025-11-20 10:25:29 +0200

    Update equation_utils.py

--- equation_utils.py
+++ equation_utils.py
@@ -1,5 +1,6 @@
 # Add the import statements for necessary sympy functions here
-
+import sympy
+from sympy import symbols, Eq, solve as sympy_solve
 
 ELEMENTS = [
     'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
